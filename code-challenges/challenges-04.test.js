07454a98a5e9591b26d24e4d8713206bd84347fd
Omar-zoubi <o.fandi02@gmail.com>
2021-04-01 15:39:05 +0300

    ch4

--- code-challenges/challenges-04.test.js
+++ code-challenges/challenges-04.test.js
@@ -8,11 +8,6 @@ Write a function called addTwo that takes in an array and adds two to every valu
 
 const addTwo = (arr) => {
   // Solution code here...
-  const plusTowArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    plusTowArr.push(arr[i] + 2);
-  }
-  return plusTowArr;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -24,8 +19,6 @@ Write a function named containsW that takes in a string. This function should us
 
 const containsW = (str) => {
   // Solution code here...
-  let lowerCase = /[w]/;
-  return lowerCase.test(str);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -42,8 +35,6 @@ For example:
 
 const isNum = (input) => {
   // Solution code here...
-  let regEx = /\d/;
-  return regEx.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -55,8 +46,6 @@ Write a function named containsWorld that takes in a string or number of any len
 
 const containsWorld = (input) => {
   // Solution code here...
-  let regEx = /world/;
-  return regEx.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -69,9 +58,6 @@ Return an array containing all the matches.
 
 const isCapitalized = (str) => {
   // Solution code here...
-  const regEx = /\b[A-Z]\w*/g;
-  const upper = str.match(regEx);
-  return upper === null ? [] : upper;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -82,12 +68,6 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 
 const citiesAtoJ = (arr) => {
   // Solution code here...
-  let newArr = [];
-  const re = /^[A-J]/;
-  arr.forEach(element => {
-    if (re.test(element)) { newArr.push(element); }
-  });
-  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -210,7 +190,7 @@ describe('Testing challenge 5', () => {
   test('It should only return words that begin with a capital letter', () => {
     const capitalResult = isCapitalized('We only want to Return the Words that begin With a capital Letter');
 
-    expect(capitalResult).toStrictEqual(['We', 'Return', 'Words', 'With', 'Letter']);
+    expect(capitalResult).toStrictEqual([ 'We', 'Return', 'Words', 'With', 'Letter' ]);
     expect(capitalResult.length).toStrictEqual(5);
 
     expect(isCapitalized('Given by our hand in the meadow that is called Runnymede, between Windsor and Staines, on the fifteenth day of June in the seventeenth year of our reign (i.e. 1215: the new regnal year began on 28 May).')).toStrictEqual(['Given', 'Runnymede', 'Windsor', 'Staines', 'June', 'May']);
@@ -257,7 +237,7 @@ xdescribe('Testing challenge 8', () => {
   const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia vel massa sed egestas. Nunc faucibus iaculis elit, a scelerisque enim condimentum sed. Aenean ac scelerisque sem, et pharetra diam.';
 
   test('It should only return words that are immediately followed by a space', () => {
-    expect(noPunctuation(lorem)).toStrictEqual(['Lorem ', 'ipsum ', 'dolor ', 'sit ', 'consectetur ', 'adipiscing ', 'Cras ', 'lacinia ', 'vel ', 'massa ', 'sed ', 'Nunc ', 'faucibus ', 'iaculis ', 'a ', 'scelerisque ', 'enim ', 'condimentum ', 'Aenean ', 'ac ', 'scelerisque ', 'et ', 'pharetra ']);
+    expect(noPunctuation(lorem)).toStrictEqual([ 'Lorem ', 'ipsum ', 'dolor ', 'sit ', 'consectetur ', 'adipiscing ', 'Cras ', 'lacinia ', 'vel ', 'massa ', 'sed ', 'Nunc ', 'faucibus ', 'iaculis ', 'a ', 'scelerisque ', 'enim ', 'condimentum ', 'Aenean ', 'ac ', 'scelerisque ', 'et ', 'pharetra ' ]);
     expect(noPunctuation(lorem).length).toStrictEqual(23);
     expect(noPunctuation('Given by our hand in the meadow that is called Runnymede, between Windsor and Staines, on the fifteenth day of June in the seventeenth year of our reign (i.e. 1215: the new regnal year began on 28 May).')).toEqual(expect.arrayContaining(['Given ', 'by ', 'our ', 'hand ', 'in ', 'the ', 'meadow ', 'that ', 'is ', 'called ', 'between ', 'Windsor ', 'and ', 'on ', 'the ', 'fifteenth ', 'day ', 'of ', 'June ', 'in ', 'the ', 'seventeenth ', 'year ', 'of ', 'our ', 'reign ', 'the ', 'new ', 'regnal ', 'year ', 'began ', 'on ', '28 ']));
   });
