e50e55373e91359ad55dffc1fc6660ea382cdabf
Omar-zoubi <o.fandi02@gmail.com>
2021-06-14 23:37:31 +0300

    sd

--- python/tests/test_merg_sort.py
+++ python/tests/test_merg_sort.py
@@ -1,19 +1,36 @@
 from code_challenges.merg_sort.merg_sort import *
-import pytest
-def test_merge():
-    tst = merge_sort([6,2,7,0])
-    actual = tst
-    excpected = [0,2,6,7]
-    assert excpected == actual
-def test_almost_sorted():
-    actual = merge_sort([1,2,3,4,6,5])
-    excpected = [1,2,3,4,5,6]
-    assert excpected == actual
-def test_merge_negativ():
-    actual = merge_sort([0,12,7,8,-12])
-    excpected = [-12,0,7,8,12]
-    assert excpected == actual
-def test_equl():
-    actual = merge_sort([2,3,3,7,7,3])
-    excpected = [2,3,3,3,7,7]
-    assert excpected == actual
+
+def test_sort():
+    lis = [2,15,33,4,66,8]
+    merge_sort(lis)
+    assert lis == [2,4,8,15,33,66]
+def test_reverse():
+    lis = [50,39,28,17,6]
+    merge_sort(lis)
+    assert lis == [6,17,28,39,50]
+
+def test_negative():
+    lis = [6,-1,0,-0.5,1,-6]
+    merge_sort(lis)
+    assert lis == [-6,-1,-0.5,0,1,6]
+def test_uniques():
+    lis = [5,12,7,5,5,7]
+    merge_sort(lis)
+    assert lis == [5,5,5,7,7,12]
+def test_one_item():
+    lis = [0]
+    merge_sort(lis)
+    assert lis == [0]
+def test_tow_item():
+    lis = [1,0]
+    merge_sort(lis)
+    assert lis == [0,1]
+
+def test_nearly_sorted():
+    lis = [2,3,5,7,13,11]
+    merge_sort(lis)
+    assert lis == [2,3,5,7,11,13]
+def test_alreaedy_sorted():
+    lis = [1,2,3,4,5]
+    merge_sort(lis)
+    assert lis == [1,2,3,4,5]
